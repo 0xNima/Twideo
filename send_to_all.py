97baee41c2118082dcc0bb7c8e4a792b11dbdebe
0xNima <neemaiox@gmail.com>
2023-02-03 00:22:48 +0330

    queue base bulk notification script

--- send_to_all.py
+++ send_to_all.py
@@ -2,83 +2,84 @@ from dotenv import load_dotenv
 
 import asyncio
 import aiohttp
-import json
 import os
+import psycopg
+import redis
 
 load_dotenv()
 
-with open('users.json', 'r') as fd:
-    json_data = json.load(fd)
+cache = redis.Redis(db=10)
+pipe = cache.pipeline()
+uids = []
 
-with open('users2.json', 'r') as fd:
-    other_json = json.load(fd)
+with psycopg.connect(os.environ.get('DATABASE_URL')) as conn:
+    with conn.cursor() as cur:
+        for record in cur.execute("SELECT chat_id FROM users").fetchall():
+            uid = record[0]
+            uids.append(uid)
+            pipe.hset('uids', uid, 1)
 
-
-all_users = set()
-
-for data in [other_json, json_data]:
-    for item in data['values']:
-        all_users.add(item[0])
-
-sent = []
-
-failure = []
-
-users = filter(lambda x: x not in sent, all_users)
-
-async def send_request(url, payload):
-    async with aiohttp.ClientSession() as session:
-        async with session.post(url=url, data=payload) as response:
-            try:
-                if response.status != 200:
-                    print(payload['chat_id'], ": ", await response.text())
-                    failure.append(payload['chat_id'])
-                else:
-                    print(payload['chat_id'], ",")
-                    sent.append(payload['chat_id']);
-            except Exception as e:
-                failure.append(payload['chat_id'])
+pipe.execute()
 
 
-URL = "https://api.telegram.org/bot{}/sendMessage"
-VIDEO_MSG_URL = "https://api.telegram.org/bot{}/sendAnimation"
-
+URL = f'https://api.telegram.org/bot{os.environ.get("TELOXIDE_TOKEN")}/sendMessage'
 
 TEXT = """
-🔔 🔔 🔔
-
-📌
-
-🔵
-🔵
-🔴
 """
 
 
-async def main():
-    tasks = []
-    chunk = 100
-    for peer_id in users:
-        _payload = {
-            "chat_id": peer_id,
-            "caption": TEXT,
-            "parse_mode": "html",
-            "disable_web_page_preview": True,
-            "animation": ""
-        }
-        tasks.append(send_request(VIDEO_MSG_URL.format(os.getenv("TELOXIDE_TOKEN2")), _payload))
+async def send_request(peer_id):
+    _payload = {
+        "chat_id": peer_id,
+        "text": TEXT,
+        "parse_mode": "html",
+        "disable_web_page_preview": True,
+    }
+    async with aiohttp.ClientSession() as session:
+        async with session.post(url=URL, data=_payload) as response:
+            try:
+                if response.status == 200:
+                    cache.hdel('uids', peer_id)
+                    print(f'requests is sent to {peer_id}')
+                    return None
+                msg = f'requests failed {peer_id}. status: {response.status}'
+            except Exception as e:
+                msg = f'requests failed {peer_id}. reason: {e}'
+            
+            print(msg)
+            return peer_id
 
-    for i in range(0, len(tasks), chunk):
-        chunked = tasks[i: i+chunk]
-        await asyncio.gather(*chunked)
-        await asyncio.sleep(1)
+
+async def main():
+    CHUNK = 100
+    
+    tasks = asyncio.Queue(len(uids))
+    
+    for peer_id in uids:
+        await tasks.put(send_request(peer_id))
+
+    while not tasks.empty():
+        print(f'running tasks again... - queue size: {tasks.qsize()}')
+
+        _chunked = []
+
+        for _ in range(CHUNK):
+            try:
+                _chunked.append(tasks.get_nowait())
+            except asyncio.QueueEmpty:
+                continue
+
+        if _chunked:
+            result = await asyncio.gather(*_chunked)
+            for response in result:
+                if isinstance(response, int):
+                    await tasks.put(send_request(response))
+
+        await asyncio.sleep(5)
+
 
 if __name__ == '__main__':
     try:
         asyncio.get_event_loop().run_until_complete(main())
     except:
         print ("_________________________ ERROR _________________________")
-
-    print("failure: \n", failure)
-    print("\n\n")
-    print("sent \n", sent)
